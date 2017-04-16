4f3a2d050e4f327ca3594d5408d84e06af162182
Adalberto Fernandes Jr <adalberto.fernandesjr@gmail.com>
2017-04-16 15:21:42 -0300

    feat: Passando piada para biblioteca android e habilitando data binding.

--- app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
@@ -1,13 +1,15 @@
 package com.udacity.gradle.builditbigger;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.Toast;
 
 import com.udacity.gradle.builditbigger.jokes.Joker;
+
+import br.com.nanodegree.adalbertofjr.jokeview.ui.JokerActivity;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -42,8 +44,10 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void tellJoke(View view) {
-        Joker joker = new Joker();
-        Toast.makeText(this, joker.getJoke(), Toast.LENGTH_SHORT).show();
+        String joke = new Joker().getJoke();
+        Intent intent = new Intent(this, JokerActivity.class);
+        intent.putExtra(Intent.EXTRA_TEXT, joke);
+        startActivity(intent);
     }
 
 }
