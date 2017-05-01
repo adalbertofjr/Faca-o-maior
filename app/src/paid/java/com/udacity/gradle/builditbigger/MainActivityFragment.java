ea0b1eb92da1c65c96a9cbbcfb9f8c5ef03fa0d9
adalbertofjr <adalberto@sysmobil.com.br>
2017-05-01 12:58:36 -0300

    task: Removido anúncio da versão paga.

---
+++ app/src/paid/java/com/udacity/gradle/builditbigger/MainActivityFragment.java
@@ -0,0 +1,26 @@
+package com.udacity.gradle.builditbigger;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * MainActivityFragment
+ * Created by Adalberto Fernandes Júnior on 01/05/17.
+ * Copyright © 2016. All rights reserved.
+ */
+
+public class MainActivityFragment extends Fragment {
+    public MainActivityFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View root = inflater.inflate(R.layout.fragment_main, container, false);
+
+        return root;
+    }
+}
